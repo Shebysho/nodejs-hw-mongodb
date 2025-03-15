869b1f1d573ebc4c22be29d9e3e49ed965fac04e
Aargo911 <jekaboy2019@gmail.com>
2025-03-15 14:40:52 +0200

    Added GET by ID and DELETE endpoints

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,13 +1,22 @@
 import express from "express";
-import { getContacts, addContact, updateContact } from "../controllers/contactsController.js";
+import {
+  getContacts,
+  getContactById,
+  addContact,
+  updateContact,
+  deleteContact,
+} from "../controllers/contactsController.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { isValidId } from "../middlewares/isValidId.js";
 import contactSchema from "../schemas/contactSchema.js";
 
 const router = express.Router();
 
-router.get("/", getContacts);
-router.post("/contacts", validateBody(contactSchema), addContact);
-router.patch("/:contactId", isValidId, validateBody(contactSchema), updateContact);
+router.get("/", getContacts); 
+router.get("/:contactId", isValidId, getContactById); 
+router.post("/", validateBody(contactSchema), addContact); 
+router.patch("/:contactId", isValidId, validateBody(contactSchema), updateContact); 
+router.delete("/:contactId", isValidId, deleteContact);
 
 export default router;
+
