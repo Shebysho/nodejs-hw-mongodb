7d8c1f567aef60da8baeae1f6bad701a9e7c329d
Aargo911 <jekaboy2019@gmail.com>
2025-03-15 22:13:15 +0200

    dl

--- src/models/contact.js
+++ src/models/contact.js
@@ -1,32 +1,12 @@
 import mongoose from "mongoose";
 
-const contactSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    phoneNumber: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-    },
-    isFavourite: {
-      type: Boolean,
-      default: false,
-    },
-    contactType: {
-      type: String,
-      required: true,
-      enum: ["personal", "home", "work"],
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+const contactSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+}, { versionKey: false, timestamps: true });
 
-export const Contact = mongoose.model("Contact", contactSchema);
+const Contact = mongoose.model("Contact", contactSchema);
+
+export default Contact; 
 
