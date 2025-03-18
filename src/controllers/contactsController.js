c6e069c3b6f393cf4fcb8ab003e5c49f08517217
Aargo911 <jekaboy2019@gmail.com>
2025-03-18 15:34:42 +0200

    d

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -1,10 +1,11 @@
 import Contact from "../models/Contact.js";
 import HttpError from "../helpers/HttpError.js";
+import { isValidObjectId } from "mongoose";
 
 export const getAllContacts = async (req, res, next) => {
     try {
         const contacts = await Contact.find();
-        res.json({ status: "success", message: "Contacts retrieved", data: contacts });
+        res.json({ status: "success", message: "Contacts retrieved", contacts });
     } catch (error) {
         next(error);
     }
@@ -13,11 +14,14 @@ export const getAllContacts = async (req, res, next) => {
 export const getContactById = async (req, res, next) => {
     try {
         const { id } = req.params;
+        if (!isValidObjectId(id)) {
+            throw new HttpError(400, "Invalid ID format");
+        }
         const contact = await Contact.findById(id);
         if (!contact) {
             throw new HttpError(404, "Contact not found");
         }
-        res.json({ status: "success", message: "Contact found", data: contact });
+        res.json({ status: "success", message: "Contact found", contact });
     } catch (error) {
         next(error);
     }
@@ -26,7 +30,7 @@ export const getContactById = async (req, res, next) => {
 export const addContact = async (req, res, next) => {
     try {
         const newContact = await Contact.create(req.body);
-        res.status(201).json({ status: "success", message: "Contact created", data: newContact });
+        res.status(201).json({ status: "success", message: "Contact created", contact: newContact });
     } catch (error) {
         next(error);
     }
@@ -35,11 +39,14 @@ export const addContact = async (req, res, next) => {
 export const updateContact = async (req, res, next) => {
     try {
         const { id } = req.params;
+        if (!isValidObjectId(id)) {
+            throw new HttpError(400, "Invalid ID format");
+        }
         const updatedContact = await Contact.findByIdAndUpdate(id, req.body, { new: true });
         if (!updatedContact) {
             throw new HttpError(404, "Contact not found");
         }
-        res.json({ status: "success", message: "Contact updated", data: updatedContact });
+        res.json({ status: "success", message: "Contact updated", contact: updatedContact });
     } catch (error) {
         next(error);
     }
@@ -48,6 +55,9 @@ export const updateContact = async (req, res, next) => {
 export const deleteContact = async (req, res, next) => {
     try {
         const { id } = req.params;
+        if (!isValidObjectId(id)) {
+            throw new HttpError(400, "Invalid ID format");
+        }
         const deletedContact = await Contact.findByIdAndDelete(id);
         if (!deletedContact) {
             throw new HttpError(404, "Contact not found");
--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,14 @@
-const errorHandler = (err, req, res, next) => {
-  const { status = 500, message = "Internal Server Error" } = err;
-  res.status(status).json({ status, message });
-};
+import Joi from "joi";
 
-module.exports = errorHandler;
+export const contactValidationSchema = Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+    email: Joi.string().email().required()
+});
+
+export const validateBody = (schema) => (req, res, next) => {
+    const { error } = schema.validate(req.body, { abortEarly: false });
+    if (error) {
+        return res.status(400).json({ status: "error", message: error.details.map((d) => d.message).join(", ") });
+    }
+    next();
+};
